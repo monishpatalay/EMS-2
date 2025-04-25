15d260b96216eb7e4473b93ddfe53e8e69736eee
MONISH PATALAY <106901576+monishpatalay@users.noreply.github.com>
2025-04-25 13:37:47 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import PlacePage from './pages/PlacePage.jsx';
 import BookingsPage from './pages/BookingsPage.jsx';
 import BookingPage from './pages/BookingPage.jsx';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://ems-2-v9qq.onrender.com';
 axios.defaults.withCredentials = true;
 
 function App() {
