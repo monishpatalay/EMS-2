557d7abd2ade562d87c7020d8e1fb4a982668e6c
MONISH PATALAY <106901576+monishpatalay@users.noreply.github.com>
2025-04-25 13:38:11 -0700

    Update Image.jsx

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image({src,...rest}) {
     src = src && src.includes('https://')
       ? src
-      : 'http://localhost:3000/uploads/'+src;
+      : 'https://ems-2-v9qq.onrender.com/uploads/'+src;
     return (
       <img {...rest} src={src} alt={''} />
     );
